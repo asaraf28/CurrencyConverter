6d1a997077e97f9407d16940cd7b7c0b7c5eba8d
Steve Lacey <steve@stevelacey.net>
2010-10-30 14:53:07 +0100

    Added demo environment detection and method to corect various UWE globals vars.

--- config/ProjectConfiguration.class.php
+++ config/ProjectConfiguration.class.php
@@ -6,13 +6,26 @@ sfCoreAutoload::register();
 class ProjectConfiguration extends sfProjectConfiguration {
   public function setup() {
     $this->enableAllPluginsExcept('sfPropelPlugin');
+
+    if(strpos($_SERVER['HTTP_HOST'], 'uwe.ac.uk') !== false) {
+      ProjectConfiguration::setUWEGlobals();
+    }
   }
 
   public function getEnvironment() {
-    if (strpos($_SERVER ['HTTP_HOST'], '.stevelacey.net' ) !== false) {
+    if(strpos($_SERVER['HTTP_HOST'], '.stevelacey.net' ) !== false) {
       return 'dev';
+    } else if(strpos(urldecode($_SERVER['REQUEST_URI']), '/~slacey/convert') !== false) {
+      return 'demo';
     } else {
       return 'live';
     }
+  }
+
+  public function setUWEGlobals() {
+    // Standardise UWE variables
+    $_SERVER['REQUEST_URI'] = str_replace(array('/~slacey', '/%7Eslacey', '/convert', '/conv'), '', $_SERVER['REQUEST_URI']);
+    $_SERVER['HTTP_X_FORWARDED_HOST'] = current(explode(', ', $_SERVER['HTTP_X_FORWARDED_HOST']));
+    $_SERVER['HTTP_X_FORWARDED_SERVER'] = current(explode(', ', $_SERVER['HTTP_X_FORWARDED_SERVER']));
   }
 }
