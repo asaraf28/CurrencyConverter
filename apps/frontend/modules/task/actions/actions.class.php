da783f3de7d2b06ab457c5c50c8b3d1a14233527
Steve Lacey <steve@stevelacey.net>
2010-10-31 19:07:01 +0000

    Re-factored toolkit to task module, executable via web (UWE servers lack PHP5 CLI).

---
+++ apps/frontend/modules/task/actions/actions.class.php
@@ -0,0 +1,14 @@
+<?php
+
+class taskActions extends myActions {
+  public function executeCc(sfWebRequest $request) {
+    sfToolkit::clearDirectory(sfConfig::get('sf_cache_dir'));
+    $this->redirect('homepage');
+  }
+
+  public function executeGetCurrencyCodes(sfWebRequest $request) {
+    chdir(sfConfig::get('sf_root_dir')); // Trick plugin into thinking you are in a project directory
+    $task = new getCurrencyCodesTask(sfContext::getInstance()->getEventDispatcher(), new sfFormatter());
+    $task->run(array(), array());
+  }
+}
--- apps/frontend/modules/toolkit/actions/actions.class.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-class toolkitActions extends sfActions {
-  public function executeClearCache(sfWebRequest $request) {
-    sfToolkit::clearDirectory(sfConfig::get('sf_cache_dir'));
-    $this->redirect('homepage');
-  }
-}
