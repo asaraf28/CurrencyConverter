e92c8c1197df498402c8e87a18d398e206260c88
Steve Lacey <steve@stevelacey.net>
2010-12-02 08:32:44 +0000

    Fixed bug in error template.

--- apps/frontend/modules/default/actions/actions.class.php
+++ apps/frontend/modules/default/actions/actions.class.php
@@ -3,6 +3,6 @@
 class defaultActions extends myActions {
   public function executeError404() {
     $this->code = 1000;
-    $this->message = sfConfig::get('app_error_1000'.);
+    $this->message = sfConfig::get('app_error_1000');
   }
 }
