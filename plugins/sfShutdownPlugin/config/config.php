30650399816468c7bcb5b408b105afb474ddf0c4
Steve Lacey <steve@stevelacey.net>
2010-12-07 17:18:42 +0000

    Removed sfShutdownPlugin, shutdown functions don't work like they used to.

--- plugins/sfShutdownPlugin/config/config.php
+++
@@ -1,4 +0,0 @@
-<?php
-// This is the most important step - the point is to include it before the Symfony classes register their shutdown methods
-$sf_shutdown_loaded_by_config = 1;
-require_once(dirname(__FILE__).DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'lib'.DIRECTORY_SEPARATOR.'sfShutdown.class.php');
