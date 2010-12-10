58820ea514f3253ea3a3db19d7da2facfd798d83
Steve Lacey <steve@stevelacey.net>
2010-12-10 02:00:08 +0000

    Fully implemented 3000 and 3100 errors.

--- config/error/error.xml.php
+++ config/error/error.xml.php
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="<?php echo sfConfig::get('sf_charset', 'UTF-8') ?>"?>
 <conv>
-  <error code="<?php echo $code ?>"><?php echo $message ?></error>
+  <error code="1000"><?php echo sfConfig::get('app_error_1000') ?></error>
 </conv>
--- config/error/exception.xml.php
+++ config/error/exception.xml.php
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="<?php echo sfConfig::get('sf_charset', 'UTF-8') ?>"?>
 <conv>
-  <error code="<?php echo $code ?>"><?php echo $message ?></error>
+  <error code="3100"><?php echo sfConfig::get('app_error_3100') ?></error>
 </conv>
